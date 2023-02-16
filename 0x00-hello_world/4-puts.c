6c08d2a8219f72ec8ce996ef7f51ccc725d5558f
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-16 15:41:34 -0800

    first code using c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,13 @@
  *
  * Printing with puts fulction
  *
- * return always 0 (success)
+ * return: Always 0 (success)
  * 
  */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 		return(0);
 
 }	
