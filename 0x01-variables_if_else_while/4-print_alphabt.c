080063a2dfb6f1509feaa7e8e7bb931769557364
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-17 12:49:02 -0800

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: always 0 (success)
+ *
+ * Description: print lowercase alphabet except q and e
+ */
+int main(void)
+{
+	char Alphabet[] = "bcdefghijklmnopqrstuvwxyz";
+	int a = 0;
+	
+	while (a < 26)
+	{
+		if ((Alphabet[a] == 'q') ||( Alphabet[a] == 'e'))
+		{
+			a++;
+			continue;
+		}
+		else
+		{
+			putchar(Alphabet[a]);
+			a++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
