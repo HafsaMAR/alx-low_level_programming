8834095587fab8e7bc709da97fb03e37a308238d
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-17 13:10:25 -0800

    display list of digit in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Always 0 (success)
+ *
+ * Description : print array of digit in base 10
+ */
+int main(void)
+{
+	int Numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int x = 0;
+	
+	while (x < 10)
+	{
+		printf("%d", Numbers[x]);
+			x++;
+	}
+	printf("\n");
+	return (0);
+}
