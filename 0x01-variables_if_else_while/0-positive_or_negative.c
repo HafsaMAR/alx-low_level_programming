84f8d0b0cd5b321a3ad5924d1265ef3dce4f3e87
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-17 08:11:21 -0800

    random variables sign

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/* main - Entry
+ * Return: Always 0 (success)
+ * Description: positive or zero or negative variables */ 
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0)	
+	if (n > 0)	
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
-
 }
