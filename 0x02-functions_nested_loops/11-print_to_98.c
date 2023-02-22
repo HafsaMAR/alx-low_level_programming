a4c7b0783712263a56146a6cdc4466b5eb229f47
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-21 17:55:23 -0800

    printing number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,14 +25,14 @@ void print_to_98(int n)
 		}
 	}
 	else if (n > 98)
-		while (n >= 98)
+		while (n > 98)
 		{
 			if (n < 100)
 			{
 				_putchar('0' + (n / 10));
 				_putchar('0' + (n % 10));
 			}
-			else 
+			else
 			{
 				_putchar('0' + (n / 100));
 				_putchar('0' + (((n - ((n / 100) * 100))) / 10));
