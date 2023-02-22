9e58859c950948586abfcc97d4d117b8a8fcc7e4
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-21 16:23:55 -0800

    display the jack bauer time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * jack_bauer - a function that prints every minute of the day of Jack Bauer
- * @h: hour parameter
- * @m: minute parameter
  * Return: void
  */
 void jack_bauer(void)
@@ -10,9 +8,9 @@ void jack_bauer(void)
 	int h = 0;
 	int m = 0;
 
-	while(h < 24)
+	while (h < 24)
 	{
-		while(m <60)
+		while (m < 60)
 		{
 			_putchar('0' + (h / 10));
 			_putchar('0' + (h % 10));
