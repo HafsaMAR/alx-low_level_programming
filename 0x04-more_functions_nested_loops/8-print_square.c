c9e9fcbfc392aae4a6868242140fb72340223f52
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-23 13:47:38 -0800

    nested loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print a square of #'s given size
+ * @size: size to draw
+ */
+void print_square(int size)
+{
+	int lign, column;
+	
+	if (size > 0)
+	{
+		for (lign = 1; lign <= size; lign++)
+		{
+			for (column = 1; column <= size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
