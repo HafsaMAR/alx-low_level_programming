6e9b3c0e1a5bad3901b2f4b35a412deebfee2444
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-23 13:01:21 -0800

    betty solving

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - a function that prints the numbers from 0 to 9
  * Description: print numbers from 0 to 9 followed by a new line
  * Return: void
  */
