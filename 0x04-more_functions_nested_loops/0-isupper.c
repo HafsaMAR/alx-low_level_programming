31d8551305df4f71457409c978e7756f5f595ee6
HafsaMAR <marouche.hafsa@gmail.com>
2023-02-23 07:59:55 -0800

    detect uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: character input
+ * Return: 1 if uppercase character, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if  ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else 
+		return (0);
+}
