fc3cdf3eb28691a2110e0d5530c17357265d8eda
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-07 11:26:29 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 
 /**
  * main - prints "and that piece of art is useful" - Dora Kprpar, 2015-10=19",
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
