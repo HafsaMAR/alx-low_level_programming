4c79f11532434e7bb3df3cae3fa154bf32a53c31
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-08 00:56:16 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: input character
+ * Return: 0 always sucess
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+0x08-recursion : We need to go deeper
