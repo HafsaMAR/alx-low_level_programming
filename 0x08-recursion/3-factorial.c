7b69bfbe4d605abc98d95ef7de918a44f8b15846
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-08 08:28:10 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,8 +10,11 @@ int factorial(int n)
 	int fact;
 
 	if (n < 0)
+	{
 		return (-1);
-	factorial(0) = 1;
-	factorial(1) = 1;
+	}
+	factorial(0) == 1;
+	factorial(1) == 1;
 	fact = n * factorial(n - 1);
+	return (fact);
 }
