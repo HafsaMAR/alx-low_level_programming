561c9eeadd7bad6152d07c5c971473f2dad5a56c
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-10 13:44:16 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints the number of arguments passed into it
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
