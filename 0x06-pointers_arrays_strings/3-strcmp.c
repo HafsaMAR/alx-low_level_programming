48667ec3d5446d63951afd7ee8c1c7fd0a7ebe5f
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-19 14:01:07 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * strcmp - function that compares two strings
+ * _strcmp - function that compares two strings
  * @s1: input
- * @s2: input 
+ * @s2: input
  * Return: negative if s1 < s2
  * 0 if s1 = s2
  * positive if s1 > s2
