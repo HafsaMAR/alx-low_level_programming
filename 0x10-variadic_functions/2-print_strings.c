da65e780e93ebecdf9f1a4522c5c2cd184b0d7ae
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-23 09:30:56 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%s", string);
+			if (i < n - 1)
+				printf("%s ", string);
+			else 
+				printf("%s", string
 		}
 	}
 	va_end(x);
