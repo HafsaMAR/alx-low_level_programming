9b118e05e066cb2a1ad132361f4d5225be770bca
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-23 03:56:32 -0700

    sum of variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum;
-	
+
 	if (n == 0)
 		return (0);
-	va_start (ap, n);
+	va_start(ap, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 	va_end(ap);
 	return (sum);
 }
