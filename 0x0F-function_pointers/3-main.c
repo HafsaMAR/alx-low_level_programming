43797e8468a942ab9bd0561cc368ee28a7041f35
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-29 03:14:36 -0700

    program for simple operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,7 +7,7 @@
  * main - entry point
  * Description: program that performs simple operations
  * @argc: counter agrument
- * @agrv: vector argument
+ * @argv: vector argument
  * Return: result of operation
  */
 int main(int argc, char *argv[])
@@ -18,18 +18,18 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error \n");
-		exit (98);
+		printf("Error\n");
+		exit(98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	s = argv[2];
 	if (get_op_func(s) == NULL)
 	{
-		printf("Error \n");
-		exit (99);
+		printf("Error\n");
+		exit(99);
 	}
 	result = get_op_func(s)(num1, num2);
 	printf("%d\n", result);
-	return(0);
+	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -45,8 +45,8 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error \n");
-		exit (100);
+		printf("Error\n");
+		exit(100);
 	}
 	return (a / b);
 }
@@ -60,9 +60,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
-        {
-                printf("Error \n");
-                exit (100);
-        }
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
