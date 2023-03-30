8dbdcabc39ee4511f74f3069a3382c8d70b8367e
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-30 10:12:39 -0700

    guard_headerfile

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,3 +20,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+#endif /*LISTS_H*/
