0f25189f194dad9c78d39285495a4eb381377ade
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-30 11:07:41 -0700

    number of nodes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,7 +8,6 @@
  * @h: pointer to the singly list
  * Return: number of elements
  */
-/*size_t print_list(const list_t *h);*/
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
@@ -20,27 +19,3 @@ size_t list_len(const list_t *h)
 	}
 	return (i);
 }
-
-/**
- * print_list - prints all elements of a list_t list.
- * @h: pointer to list_t list
- * Return: number of node
- */
-/*
-size_t print_list(const list_t *h)
-{
-	int i = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
-	}
-	return (i);
-}*/
