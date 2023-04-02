cc8ad8d3ec2a6f8f4793225502e431c9db9e0f6f
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-02 06:07:27 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,13 +17,10 @@ int main(void)
 	for (count = 0; count < 92; count++)
 	{
 	sum = fib1 + fib2;
+	printf("%lu,", sum);
 	if (count < 91)
 	{
-		printf("%lu, ", sum);
-	}
-	if (count = 91)
-	{
-		printf("%lu, ", sum);
+		printf(" ");
 	}
 	fib1 = fib2;
 	fib2 = sum;
