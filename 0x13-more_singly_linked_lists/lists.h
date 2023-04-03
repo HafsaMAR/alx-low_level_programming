9136d37712ca5b779d3238830f7a7a91a14bc8e8
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-03 09:06:24 -0700

    headermodification

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,11 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * 
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
 	int n;
@@ -26,3 +27,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+#endif /*LISTS_H*/
