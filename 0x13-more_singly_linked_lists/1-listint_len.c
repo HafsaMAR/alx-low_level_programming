86b6b644d42cba7a5e664159fbc82ff0cf8b5cc2
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-03 06:20:01 -0700

    Number of element in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "list.h"
+
+/**
+ * listint_len - Return the number of element in list
+ * @h: pointer to listint_t
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+	{
+		return(0);
+	}
+	return(1 + listint_len(h->next));
+}
