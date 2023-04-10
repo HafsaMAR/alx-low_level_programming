5cadf75c6373a9685768f588c920344d9f24106d
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-09 23:28:04 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,43 +1,3 @@
-#include <stdlib.h>
-
-/**
- * power - return the power of numbe by exp
- * @num: unput number
- * @ex: power
- * Return: value
- */
-
-unsigned long int power(int num, unsigned long int ex)
-{
-	unsigned int i, result;
-
-	result = 1;
-	for (i = 0; i < ex; i++)
-	{
-		if (ex == 0)
-			return (1);
-		result *= num;
-	}
-	return (result);
-}
-
-/**
- * greatest_2_exp - gives the greatest power of 2 less that n
- * @n: input number
- * Return: exp of 2
- */
-
-unsigned long int greatest_2_exp(unsigned long int n)
-{
-	unsigned long int i = 1;
-
-	while (n >= power(2, i))
-	{
-		i++;
-	}
-	return (i);
-}
-
 /**
  * get_bit - returns the value in the given index
  * @n: input number
@@ -46,10 +6,9 @@ unsigned long int greatest_2_exp(unsigned long int n)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max, mask = 1UL << index;
+	unsigned long int mask = 1UL << index;
 
-	max = greatest_2_exp(n);
-	if (index >= max)
+	if (index > (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
