d6c2259e9ae9bec0325043038d61f376dc8cfa42
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-09 23:50:01 -0700

    update less lines

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_binary - prints binary representation of n
@@ -11,5 +11,5 @@ void print_binary(unsigned long int n)
 	{
 		print_binary(n >> 1);
 	}
-	printf("%lu", n & 1);
+	_putchar((n & 1) + '0');
 }
