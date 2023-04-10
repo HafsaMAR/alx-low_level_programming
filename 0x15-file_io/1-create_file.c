589b47b82e7ccd22fd068ffc0f5943333cda56a8
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-10 15:30:02 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * creat_file - creates a file
+ * @filename: pointer to string 'name of the file'
+ * @text_content: string terminated by NULL
+ * Return: 1 success or -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	len = strlen(text_content);
+	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
