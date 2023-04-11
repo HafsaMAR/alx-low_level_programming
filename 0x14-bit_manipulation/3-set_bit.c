101444dfae54de0c1d8575a1ab3b192f457825a4
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-11 11:07:18 -0700

    correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,55 +1,8 @@
-
-/**
- * power - return the power of numbe by exp
- * @num: unput number
- * @ex: power
- * Return: value
- */
-
-unsigned long int power(int num, unsigned long int exp)
-{
-	unsigned int i, result;
-
-	result = 1;
-	if (exp == 0)
-	{
-		return (1);
-	}
-	for (i = 0; i < exp; i++)
-	{
-		result *= num;
-	}
-	return (result);
-}
-
-/**
- * greatest_2_exp - gives the greatest power of 2 less that n
- * @n: input number
- * Return: exp of 2
- */
-
-unsigned long int greatest_2_exp(unsigned long int n)
-{
-	unsigned long int i = 1;
-
-	while (n >= power(2, i + 1))
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * set_bit - set the value at the given index to 1
- * @n: pointer to variable
- * @index: the index of digit to change
- * Return: 1 (success) -1 otherwise
- */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1UL << index;
 
-	if (!n)
+	if (!n || index > sizeof (unsigned int) * 8)
 	{
 		return (-1);
 	}
