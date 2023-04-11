b7b88bd84a9adb6e8a749a3a72a62b45b3cf2fde
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-11 11:02:03 -0700

    correction

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,42 +1,3 @@
-
-/**
- * power - return the power of numbe by exp
- * @num: unput number
- * @ex: power
- * Return: value
- */
-
-unsigned long int power(int num, unsigned long int exp)
-{
-	unsigned int i, result;
-
-	result = 1;
-	if (exp == 0)
-		return (1);
-	for (i = 0; i < exp; i++)
-	{
-		result *= num;
-	}
-	return (result);
-}
-
-/**
- * greatest_2_exp - gives the greatest power of 2 less that n
- * @n: input number
- * Return: exp of 2
- */
-
-unsigned long int greatest_2_exp(unsigned long int n)
-{
-	unsigned long int i = 0;
-
-	while (n >= power(2, i + 1))
-	{
-		i++;
-	}
-	return (i);
-}
-
 /**
  * clear_bit - set a digit at index to 0
  * @n: pointer to variable
@@ -46,9 +7,9 @@ unsigned long int greatest_2_exp(unsigned long int n)
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int comp,  max = greatest_2_exp(*n);
+	unsigned long int comp;
 
-	if (index > max)
+	if (!n)
 		return (-1);
 	comp = ~(1UL << index);
 	*n &= comp;
