4106136381b0fa9f0c634da9963754505b4d0777
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-13 04:40:06 -0700

    correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,20 +21,35 @@ int main(int argc, char *argv[])
 		exit(97);
 	fd_src = open(argv[1], O_RDONLY);
 	if (fd_src == -1)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_dest == -1)
+	{
+		printf("Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
 	b_read = read(fd_src, buffer, BUF_SIZE);
 	while (b_read > 0)
 	{
 		b_written = write(fd_dest, buffer, b_read);
 		if (b_read != b_written)
+		{
+			printf("Error: Can't write to %s\n", argv[2]);
 			exit(99);
+		}
 	}
 	if (close(fd_src) == -1)
+	{
+		printf("Error: Can't close fd %d\n", fd_src);
 		exit(100);
+	}
 	if (close(fd_dest) == -1)
+	{
+		printf("Error: Can't close fd %d\n", fd_dest);
 		exit(100);
+	}
 	return (0);
 }
