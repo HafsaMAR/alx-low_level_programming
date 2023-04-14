d6f48a2d4a7da63759edbb41d1eae64b2a97f609
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-13 23:22:10 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,13 +25,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (access(argv[2], W_OK) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+	if (fd_to == -1 || access(argv[2],W_OK) == -1)
 	{
 		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[2]);
 		exit(99);
