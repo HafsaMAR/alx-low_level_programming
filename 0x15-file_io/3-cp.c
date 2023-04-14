fe56123d9969f311fb4c232985ecc9a6fa56e94b
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-13 23:16:46 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,11 +7,6 @@
 
 #define BUFFER_SIZE 1024
 
-void print_error(char *message)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
-}
-
 int main(int argc, char *argv[])
 {
 	int close_from, close_to;
@@ -21,21 +16,24 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		print_error("Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		print_error("Can't read from file");
-		print_error(argv[1]);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
+	if (access(argv[2], W_OK) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		print_error("Can't write to file");
-		print_error(argv[2]);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[2]);
 		exit(99);
 	}
 
@@ -44,15 +42,13 @@ int main(int argc, char *argv[])
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
-			print_error("Can't write to file");
-			print_error(argv[2]);
+			dprintf(STDERR_FILENO, "Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
-		print_error("Can't read from file");
-		print_error(argv[1]);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	close_from = close(fd_from);
