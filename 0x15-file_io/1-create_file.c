2bfee10f60d70ed8759360a61e4757c992130453
HafsaMAR <marouche.hafsa@gmail.com>
2023-04-14 01:54:03 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,16 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+	{
+		goto finish;
+	}
 	len = strlen(text_content);
 	if (write(fd, text_content, len) == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	close(fd);
+finish:	close(fd);
 	return (1);
 }
