9435a9980271e1d070e71b2772afef0894cac8d6
HafsaMAR <marouche.hafsa@gmail.com>
2023-05-03 15:46:18 -0700

    error insert 1st node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,8 +21,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	current = *head;
 	if (idx == 0)
 	{
-		if (*head == NULL)
-			return (NULL);
 		*head = new_node;
 		new_node->n = n;
 		new_node->next = current;
