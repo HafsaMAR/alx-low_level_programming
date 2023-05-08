38bc61637e35c91e4c1c480c20f36d5e3343b62b
HafsaMAR <marouche.hafsa@gmail.com>
2023-05-08 13:45:22 -0700

    main files

---
+++ 0x15-file_io/header.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+#endif
