8397f05ce2a1d5ec21fa7d36db3d3adf84b9ce07
HafsaMAR <marouche.hafsa@gmail.com>
2023-05-08 16:43:58 -0700

    creat/trunc a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		goto finish;
 	len = strlen(text_content);
 	written_bytes = write(fd, text_content, len);
 	if (written_bytes == -1)
@@ -27,6 +29,6 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	close(fd);
+finish: close(fd);
 	return (1);
 }
