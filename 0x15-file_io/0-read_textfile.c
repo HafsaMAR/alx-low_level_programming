8d6e4983f5588b2edce083c14b3ca29687d8e071
HafsaMAR <marouche.hafsa@gmail.com>
2023-05-08 15:07:03 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buffer;
 	const char *ptr = filename;
-	ssize_t bytes_read, bytes_written, bytes = 0;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
@@ -27,17 +27,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-		return (0);
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	while (bytes_written > 0)
 	{
-		if (bytes_written == -1)
-			return (0);
-		bytes += bytes_written;
-		if (bytes >= (ssize_t) letters)
-			break;
-		bytes_read = read(fd, buffer, letters);
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+		free(buffer);
+		close(fd);
+		return (0);
 	}
-	return (bytes);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (bytes_written);
 }
