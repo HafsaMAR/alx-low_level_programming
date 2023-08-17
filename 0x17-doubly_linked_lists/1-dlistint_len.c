105375c033a6b1538aef31fad0b104e7f07ee917
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-17 21:08:29 +0100

    list lenght

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,15 @@
 #include "lists.h"
 
 size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *node;
+    int i = 0;
+
+    if (h == NULL)
+        return (0);
+    for (node = h; node != NULL; node = node->next)
+    {
+        i++;
+    }
+    return (i);
+}
