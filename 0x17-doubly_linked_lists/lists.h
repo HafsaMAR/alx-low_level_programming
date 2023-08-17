0c0ba25c0d03c68ca601201cc6bd1d9f2564993c
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-17 20:31:56 +0100

    HEADER BETTY

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,4 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 
 /**
  * struct dlistint_s - doubly linked list
@@ -7,7 +13,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -25,3 +31,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+#endif
