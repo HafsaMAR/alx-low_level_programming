0cce4bd0290d3271ebcd67d70ff8c6ae25425039
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-07 23:35:12 +0100

    get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves the value associated with key
+ * @ht: pointer to hash_table_t structure
+ * @key: key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    hash_node_t *node;
+    unsigned int index;
+    if (!ht || !key)
+        return (NULL);
+    index = key_index((const unsigned char *)key, ht->size);
+    node = ht->array[index];
+    while (node)
+    {
+        if (strcmp(node->key, key) == 0)
+            return (node->value);
+        node = node->next;
+    }
+    return (NULL);
+}
