156e1abbaca4ba44f7474caac0e4fc4a2edb9a2c
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-07 23:19:41 +0100

    create empty hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table;
+    unsigned int i;
 
     if (size == 0)
         return (NULL);
@@ -15,7 +16,14 @@ hash_table_t *hash_table_create(unsigned long int size)
     if (new_table == NULL)
         return (NULL);
     new_table->size = size;
-    /*new_table->array = malloc(sizeof(hash_node_t));
-    new_table->array[size] = NULL;
-    */return (new_table);
+    new_table->array = malloc(sizeof(hash_node_t) * size);
+    if (!new_table->array)
+    {
+        free(new_table);
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+        new_table->array[i] = NULL;
+    return (new_table);
 }
