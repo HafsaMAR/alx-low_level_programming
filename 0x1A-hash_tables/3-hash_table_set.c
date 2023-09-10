afdabcd067ea8b764590f8e2dad1affec95313ad
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-10 20:18:07 +0100

    return value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
             if (value == NULL)
                 return (0);
             temp->value = strdup(value);
-            return (0);
+            return (1);
         }
         temp = temp->next;
     }
