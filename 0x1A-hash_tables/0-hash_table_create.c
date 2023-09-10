a97b5f9bc6aa5d21b356d9bdec2a51c083346d53
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-10 20:06:25 +0100

    memory allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	new_table = malloc(sizeof(new_table));
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
