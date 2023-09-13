d633600848bd020cf9e68f459d916a4961e6791c
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-14 00:55:10 +0100

    set hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,5 +1,7 @@
 #include "hash_tables.h"
 
+
+
 /**
  * shash_table_create - function that create an ordered hash table
  * @size: size of the arbucket
@@ -32,6 +34,102 @@ shash_table_t *shash_table_create(unsigned long int size)
 	ht->shead = NULL;
 	ht->stail = NULL;
 	return (ht);
+}
+
+/**
+ * add_new_node - function that add new node to shash struct
+ * @h: pointer to the hrad of shash table
+ * @key: key of the new node
+ * @value: value of the new node
+ * Return: pointer to the new node (success), NULL otherwise
+*/
+
+shash_node_t *add_new_node(shash_node_t **h, const char *key, const char *value)
+{
+	shash_node_t *temp;
+
+	temp = *h;
+	while (temp)
+	{
+		if (strcmp(key, temp->key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (temp);
+		}
+		temp = temp->next;
+	}
+	temp = malloc(sizeof(hash_node_t));
+
+	if (!temp)
+		return (NULL);
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+	temp->next = *h;
+
+	*h = temp;
+	return (temp);
+}
+
+/**
+ * add_to_sorted - add node to the sorted hash table
+ * @ht: pointer to hash table
+ * @new_node: pointer to the new node
+*/
+
+void add_to_sorted(shash_table_t *ht, shash_node_t *new_node)
+{
+	shash_node_t *temp1, *temp2;
+	int n;
+
+	temp1 = temp2 = ht->shead;
+	while (temp1)
+	{
+		n = strcmp(new_node->key, temp1->key);
+		if (n == 0)
+			return;
+		else if (n < 0)
+		{
+			new_node->sprev = temp1->sprev;
+
+			if (temp1->sprev)
+				temp1->sprev->snext = new_node;
+			else
+				ht->shead = new_node;
+			
+			temp1->sprev = new_node;
+			new_node->snext = temp1;
+			return;
+		}
+		temp2 = temp1;
+		temp1 = temp1->snext;
+	}
+	new_node->sprev = temp2;
+	new_node->snext = NULL;
+
+	if (ht->shead)
+		temp2->snext = new_node;
+	else
+		ht->shead = new_node;
+	ht->stail = new_node;
+}
+
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	shash_node_t *node;
+
+	if (!ht)
+		return (0);
+	if (key == NULL || *key == '\0')
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	node = add_new_node(&(ht->array[index]), key, value);
+	if (!node)
+		return (0);
+	add_to_sorted(ht, node);
+	return (1);
+
 }
 
 /**
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -84,4 +84,9 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+shash_node_t *add_new_node(shash_node_t **h, const char *key, const char *value);
+void add_to_sorted(shash_table_t *ht, shash_node_t *new_node);
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+
 #endif
