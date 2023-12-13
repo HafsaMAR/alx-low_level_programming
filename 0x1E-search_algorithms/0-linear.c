d201ac3ba3ad68d67cfac63358b26ca839ddff24
HafsaMAR <marouche.hafsa@gmail.com>
2023-12-13 15:59:19 +0100

    first commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that look for a value in array using linear search
+ * @array: pointer to the first element of the array
+ * @size: the number of elements in the array
+ * @value: the value to look for  in the array
+ * Return: the first occurence of the value in the array -1 otherwise
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	while (*array && i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+/*header file*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* functions prototype*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
