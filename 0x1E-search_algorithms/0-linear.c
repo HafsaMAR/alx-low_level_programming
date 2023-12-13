8a11b9722157b250c786680e5b19bc6e17becdab
HafsaMAR <marouche.hafsa@gmail.com>
2023-12-13 17:29:01 +0100

    CODE FIX

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
-	while (*array && array && i < (int)size)
+	while (array && i < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
